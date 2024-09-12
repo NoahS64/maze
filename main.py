e881eab4acc57b4a61f15c8ae3334b6a287230e2
NoahS64 <noahsluiman@gmail.com>
2024-09-12 11:04:37 -0400

    Fini

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 #Date: September 9th 2024
 #MAZE
 import pygame,sys
-
+import textstuff
 
 pygame.init()
 
@@ -21,6 +21,8 @@ walls=[]
 img_bgr = pygame.image.load('images/baconator.jpg') #with .png or .jpb included in the name
 img_bgr = pygame.transform.scale(img_bgr, (99, 99))  #resize image Where 35 ,35 is the size, (x,y)
 font = pygame.font.SysFont('Consolas', 30)
+
+old_high_score=float(textstuff.strings_read('txt_file.txt')[0])
 #Setup of Starting objects
 
 window = pygame.display.set_mode((WINDOW_WIDTH,WINDOW_HEIGHT), pygame.HWSURFACE)
@@ -28,10 +30,11 @@ pygame.display.set_caption("Title")
 def display():
     window.fill((0,0,0)) #White background
     global speedx, speedy, timer, bgr, finish
-    speedx+=.02*timer
-    speedy+=.02*timer
+    speedx+=.035*timer
+    speedy+=.035*timer
     timer+=.01
-    window.blit(font.render(f"{timer}", True, (123, 255, 78)), (200,160))
+    timer = float('{0:.2f}'.format(timer))
+
     
     walls.append(pygame.draw.rect(window,(255,255,255),(0,0,speedx,speedy)))
     walls.append(pygame.draw.rect(window,(255,0,255),(0,0,WINDOW_WIDTH,10)))
@@ -51,12 +54,12 @@ def display():
     walls.append(pygame.draw.rect(window,(255,0,255),(650,150,200,10)))
     walls.append(pygame.draw.rect(window,(255,0,255),(800,350,10,300)))
     walls.append(pygame.draw.rect(window,(255,0,255),(800,800,10,200)))
-    walls.append(pygame.draw.rect(window,(255,0,255),(500,900,10,200)))
+    walls.append(pygame.draw.rect(window,(255,0,255),(450,900,10,200)))
     walls.append(pygame.draw.rect(window,(255,0,255),(300,700,10,150)))
     walls.append(pygame.draw.rect(window,(255,0,255),(500,700,160,10)))
     walls.append(pygame.draw.rect(window,(255,0,255),(650,600,10,100)))
-    walls.append(pygame.draw.rect(window,(255,0,255),(0,0,0,0)))
-    walls.append(pygame.draw.rect(window,(255,0,255),(0,0,0,0)))
+    walls.append(pygame.draw.rect(window,(255,0,255),(650,600,150,10)))
+    walls.append(pygame.draw.rect(window,(255,0,255),(650,700,10,175)))
     walls.append(pygame.draw.rect(window,(255,0,255),(0,0,0,0)))
     walls.append(pygame.draw.rect(window,(255,0,255),(0,0,0,0)))
     
@@ -65,9 +68,10 @@ def display():
     
     
     bgr=window.blit(img_bgr,(player_x, player_y))
-    window.blit(font.render("BACONATOR", True, (123, 255, 78)), (0,160))
+    window.blit(font.render("FINISH", True, (123, 255, 78)), (10,850))
+    window.blit(font.render(f"HIGHSCORE: {old_high_score}", True, (123, 255, 78)), (700,200))
     
-    
+    window.blit(font.render(f"{format(timer)}", True, (123, 255, 78)), (200,160))
 def collision(object1, object2):
     return object1.colliderect(object2)
 
@@ -90,14 +94,20 @@ while True:
     movey = (key_Input[pygame.K_w]*-speed) + (key_Input[pygame.K_s]*speed)
     player_x += movex
     player_y += movey
-    #gridHelp(window,WINDOW_WIDTH,WINDOW_HEIGHT)
+    gridHelp(window,WINDOW_WIDTH,WINDOW_HEIGHT)
     
     for wall in walls:
         if collision(wall,bgr):
+            if timer>old_high_score:
+                textstuff.strings_write([str(timer)], 'txt_file.txt')
             print("Game over")
             exit()
     if collision(finish,bgr):
+        if timer>old_high_score:
+            textstuff.strings_write([str(timer)], 'txt_file.txt')
+            
         print("YOU WON!!")
+        
         exit()
     
     for event in pygame.event.get():
