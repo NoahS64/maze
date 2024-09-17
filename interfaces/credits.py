2784e652eb6a95e2b6913de15de1cb8cd09cf685
NoahS64 <noahsluiman@gmail.com>
2024-09-17 09:43:57 -0400

    did more stuff

--- interfaces/credits.py
+++ interfaces/credits.py
@@ -6,8 +6,9 @@ import manager
 
 def output(window):
     font = pygame.font.SysFont('Consolas', 30)
-    key_Input = pygame.key.get_pressed()
+    
     while True:
+        key_Input = pygame.key.get_pressed()
         window.fill((255,255,255))
         window.blit(font.render("CREDITS", True, (123, 255, 78)), (200,250))
         window.blit(font.render("CREATOR-NOAH SLUIMAN", True, (123, 255, 78)), (200,350))
@@ -16,7 +17,6 @@ def output(window):
         window.blit(font.render("PICTURES-WENDY'S", True, (123, 255, 78)), (200,500))
         
         window.blit(font.render("PRESS ESC TO CONTINUE", True, (123, 255, 78)), (200,700))
-        key_input = pygame.key.get_pressed()
         if (key_Input[pygame.K_ESCAPE]):
             break
         for event in pygame.event.get():
